5df85cb709edd798b0cc221f88ee6c08186e03b2
learner-09 <65757490+learner-09@users.noreply.github.com>
2020-06-07 20:12:30 +0530

    Update add-task.component.ts

--- src/app/core/add-task/add-task.component.ts
+++ src/app/core/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestsService } from 'src/app/shared/requests.service';
+import { ApiConstants } from 'src/app/shared/app-constant';
+import { NotifyServiceService } from 'src/app/shared/notify-service.service';
 
 @Component({
   selector: 'app-add-task',
@@ -7,9 +10,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
-  constructor() { }
+  constructor(    
+    private request: RequestsService,
+    private constants: ApiConstants,
+    private notify: NotifyServiceService) { }
 
-  ngOnInit() {
+  public addTaskObj={
+    title:null,
+    description:null,
+    createdDate:new Date(),
+    updatedDate:null,
+    startDate:null,
+    endDate:null,
+    status:3,
+    colorCode:null,
+    notifyOptContact:false,
+    notifyOptEmail:false,
+    notifyOptWeb:false,
+    urlToImage:"",
+    location:"Bengaluru",
+    locationLat:null,
+    locationLong:null,
+    user:null,
+    tagMappingList:null
   }
 
-}
+  public returnResponse: any;
+  public taskStatus: any[];
+  public taskColor: any[];
+  public taskTag: any[];
+
+  ngOnInit() {
+
+    this.request.getData(this.constants.INIT_TASK).subscribe(data => {
+      if (data.responseStatus === 'SUCCESS') {
+        this.taskColor = data.response.col;
+        this.taskTag = data.response.tag;
+        this.taskStatus = data.response.status;
+      }
+    }, error => {
+      this.notify.errorMessageByString('Some error occurred');
+    });
+  }
+  /*addUserToTask(){
+    this.addTaskObj.user=
+  }*/
+  addTasks() {
+    this.request.postData(this.constants.ADD_TASK, this.addTaskObj).subscribe(data => {
+      if (data.responseStatus === 'SUCCESS') {
+        this.returnResponse = data.response;
+      } else {
+        this.notify.errorMessageByString('Tasks cannot be Created');
+      }
+    }, error => {
+      this.notify.errorMessageByString('Some error occurred');
+    });
+  }
+
+  selectColor(color: string) {
+    this.addTaskObj.colorCode = color;
+  }
+
+  reset() {
+    this.addTaskObj={
+      title:null,
+      description:null,
+      createdDate:new Date(),
+      updatedDate:null,
+      startDate:null,
+      endDate:null,
+      status:3,
+      colorCode:null,
+      notifyOptContact:false,
+      notifyOptEmail:false,
+      notifyOptWeb:false,
+      urlToImage:"",
+      location:"Bengaluru",
+      locationLat:null,
+      locationLong:null,
+      user:null,
+      tagMappingList:[]
+    };
+  }
+  }
